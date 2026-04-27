4f396d0ff092949893b62858db458928cea34673
Ahmed ElAlfy <ahalfy2005@gmail.com>
2026-04-27 21:59:24 +0300

    chore: enable .env configuration
    add .env and .env.example

--- src/main/java/com/na7ki/backend/BackendApplication.java
+++ src/main/java/com/na7ki/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.na7ki.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
@@ -13,6 +14,10 @@ import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		// Load .env into System properties
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
