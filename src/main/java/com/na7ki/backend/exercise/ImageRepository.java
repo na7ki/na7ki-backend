ee562c8ed41e66463121eb7a15d4bf09514f86ac
mai awad <maiawad@paymob.com>
2026-05-08 22:55:20 +0300

    chore: reorganize the folder structure

--- src/main/java/com/na7ki/backend/exercise/ImageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.na7ki.backend.exercise;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
