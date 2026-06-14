c3cd28787f01eab5373ab79a117d320f3c0e1282
Ahmed ElAlfy <ahalfy2005@gmail.com>
2026-06-14 20:22:44 +0300

    feat: added API web documentation
    Swagger: http://localhost:8081/swagger-ui/index.html#/
    api-docs: http://localhost:8081/v3/api-docs
    
    check if port number changed in the future

--- src/main/java/com/na7ki/backend/core/security/SecurityConfig.java
+++ src/main/java/com/na7ki/backend/core/security/SecurityConfig.java
@@ -54,6 +54,11 @@ public class SecurityConfig {
         http.authorizeHttpRequests(auth ->
                 auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
                         .anyRequest().authenticated()
 //                        .requestMatchers(HttpMethod.GET, "/api/test/**").hasAuthority("ADMIN")
         )
