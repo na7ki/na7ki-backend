55f0f7e2fb83a16389e8b1b2ab0b10842456ecb4
mai awad <maiawad@paymob.com>
2026-06-16 23:10:11 +0300

    fix: merge conflicts

--- src/main/java/com/na7ki/backend/BackendApplication.java
+++ src/main/java/com/na7ki/backend/BackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-//exclude property to be removed when db connection info are assigned
 @SpringBootApplication
-
 public class BackendApplication {
 
 	public static void main(String[] args) {
