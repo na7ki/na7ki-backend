da5703541d8452799a46ecc4bc30de23d313577c
Ahmed ElAlfy <ahalfy2005@gmail.com>
2026-06-17 21:20:58 +0300

    fix: retreive missing Specialist data

--- src/main/java/com/na7ki/backend/account_management/dto/response/GetSpecialistProfileResponse.java
+++ src/main/java/com/na7ki/backend/account_management/dto/response/GetSpecialistProfileResponse.java
@@ -1,7 +1,10 @@
 package com.na7ki.backend.account_management.dto.response;
 
+import lombok.Data;
+
 import java.time.LocalDate;
 
+@Data
 public final class GetSpecialistProfileResponse extends GetUserProfileResponse {
 
     private String address;
