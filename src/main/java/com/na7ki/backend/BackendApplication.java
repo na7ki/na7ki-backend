3a28a268e019c1223c6d99ce4498b06fd5b4a1af
Ahmed ElAlfy <ahalfy2005@gmail.com>
2026-06-25 15:52:33 +0300

    fix: reapplied support for reading .env
    readded lines of code in BackendApplication to read .env.
    they were already put many commits ago but were removed by mistake at
    sometime

--- src/main/java/com/na7ki/backend/BackendApplication.java
+++ src/main/java/com/na7ki/backend/BackendApplication.java
@@ -1,15 +1,17 @@
 package com.na7ki.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		// Load .env into System properties
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
